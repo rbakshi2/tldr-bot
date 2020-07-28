b685cc8952dbaa485070b302a1c80783c3e35dd6
Raghav Bakshi <rabaksh@microsoft.com>
2020-07-28 13:12:43 -0700

    init setup

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = "bc930af9-b7c1-4bb9-ba6f-33d0568f7998"
+    APP_PASSWORD = ""
