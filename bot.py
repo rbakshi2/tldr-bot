74f17667822deaefef7618f7a386385372042c1d
Raghav Bakshi <rabaksh@microsoft.com>
2020-07-29 12:31:56 -0700

    using SUMY model for extractive summarization, also added custom summary length handler using regex

--- bot.py
+++ bot.py
@@ -3,20 +3,35 @@
 
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
-from sumbert import summarize
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+import re
+
 
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity("tl;dr:" + summarize(turn_context.activity.text))
-
-    async def on_members_added_activity(
-        self,
-        members_added: ChannelAccount,
-        turn_context: TurnContext
-    ):
-        for member_added in members_added:
-            if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+        LANGUAGE = "english"
+        SENTENCES_COUNT = 3
+        text = turn_context.activity.text
+        parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+        stemmer = Stemmer(LANGUAGE)
+        sentence_count_regex = re.compile(r'tldr-\d')
+        mo = sentence_count_regex.search(text.lower())
+        if mo and mo.group():
+            tokens = mo.group().split('-')
+            if len(tokens) == 2:
+                SENTENCES_COUNT = tokens[1]
+        summarizer = Summarizer(stemmer)
+        summarizer.stop_words = get_stop_words(LANGUAGE)
+        summary = ""
+        for sentence in summarizer(parser.document, SENTENCES_COUNT):
+            summary += str(sentence)
+            summary += '\n'
+        await turn_context.send_activity("tl;dr: " +  summary)
